f6668b9d5e641a787e93d2b98789bb00e4b1f508
Simon Louvet <simon.louvet.zen@gmail.com>
2021-05-19 11:41:01 +0200

    ini

---
+++ server/services/api.service.js
@@ -0,0 +1,25 @@
+const ApiGatewayService = require('moleculer-web');
+const { Routes: SparqlEndpointRoutes } = require('@semapps/sparql-endpoint');
+
+module.exports = {
+  mixins: [ApiGatewayService],
+  settings: {
+    server: true,
+    routes: [],
+    cors: {
+      origin: '*',
+      exposedHeaders: '*'
+    }
+  },
+  dependencies: [
+    'fuseki-admin',
+    'ldp',
+    'sparqlEndpoint',
+  ],
+  async started() {
+    [
+      ...(await this.broker.call('ldp.getApiRoutes')),
+      ...(await this.broker.call('sparqlEndpoint.getApiRoutes')),
+    ].forEach(route => this.addRoute(route));
+  }
+};

76f9d611f3fb047dc0f5d583cc4f24676be2ca7c
simonLouvet <simon.louvet.zen@gmail.com>
2023-12-29 16:52:43 +0100

    Update compact-cron-prod.sh

--- compact-cron-prod.sh
+++ compact-cron-prod.sh
@@ -4,15 +4,18 @@
 #PATH=/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/bin
 
 # check if the path is ok
-cd cartoresilience
+# cd cartoresilience
 
 # Stop all containers including Fuseki
-/usr/local/bin/docker-compose -f docker-compose-prod.yaml down
+# /usr/local/bin/docker-compose -f docker-compose-prod.yaml down
 
 # Launch compact service
-/usr/local/bin/docker-compose -f docker-compose-prod.yaml up -d fuseki_compact
+# /usr/local/bin/docker-compose -f docker-compose-prod.yaml up -d fuseki_compact
 
 # Restart 
-/usr/local/bin/docker-compose -f docker-compose-prod.yaml up -d
+# /usr/local/bin/docker-compose -f docker-compose-prod.yaml up -d
+
+
+make compact-prod
 
 echo "[INFO] Cron job compact finished at" $(date)
